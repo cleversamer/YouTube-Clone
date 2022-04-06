754f6ad4e3530a361c2c9aac5de3ce90e0125411
Samer A <thedev.samer@gmail.com>
2022-04-06 16:25:16 +0300

    added MaterialUI and MaterialUI Icons & started building the app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 import "./css/app.css";
 
 const App = () => {
   return (
     <div className="app">
       <h1>Hello World!</h1>
+      <Header />
+      {/* Sidebar - Recommended Videos */}
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h2>header</h2>
+    </header>
+  );
+};
+
+export default Header;
