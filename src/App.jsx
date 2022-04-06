27e8e0990b176d1b0d03ddd77004846b13328dc5
Samer A <thedev.samer@gmail.com>
2022-04-06 17:03:10 +0300

    finished building the header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "./css/app.css";
 const App = () => {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
       <Header />
       {/* Sidebar - Recommended Videos */}
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import {
+  Menu,
+  Search,
+  VideoCall,
+  Apps,
+  Notifications,
+} from "@mui/icons-material";
+import { Avatar } from "@mui/material";
 import "../css/header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <h2>header</h2>
+      <div className="header__left">
+        <Menu className="clickable" />
+
+        <a href="/">
+          <img
+            className="header__logo clickable"
+            src="img/logo.png"
+            alt="YouTube logo"
+          />
+        </a>
+      </div>
+
+      <div className="header__input-container">
+        <input
+          type="text"
+          placeholder="Search"
+          className="header__search-input"
+        />
+        <Search className="header__search-icon clickable" />
+      </div>
+
+      <div className="header__icons">
+        <VideoCall className="header__icon clickable" />
+        <Apps className="header__icon clickable" />
+        <Notifications className="header__icon clickable" />
+        <Avatar
+          className="clickable"
+          alt="Samer A."
+          src="https://avatars.githubusercontent.com/u/73291969?v=4"
+        />
+      </div>
     </header>
   );
 };
