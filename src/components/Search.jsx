12b842a90554c5582b496e5511e20dbdfe29ba0d
Samer A <thedev.samer@gmail.com>
2022-04-07 18:26:21 +0300

    put hard-coded data in a seperate file

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { TuneOutlined } from "@mui/icons-material";
 import ChannelRow from "./ChannelRow";
 import VideoRow from "./VideoRow";
+import { videos } from "../data";
 import "../css/search.css";
 
 const Search = () => {
@@ -27,16 +28,9 @@ const Search = () => {
 
       <hr />
 
-      <VideoRow
-        verified
-        title="Java for the Haters in 100 Seconds"
-        views="537K"
-        channel="Fireship"
-        timestamp="17 hours ago"
-        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam voluptatibus eum eos ipsa, repellat culpa, molestiae illum optio voluptates illo enim numquam alias unde, sint asperiores tenetur facere beatae sit."
-        posterUrl="https://i.ytimg.com/vi/m4-HM_sCvtQ/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDQxtCvGWd0nYzijJ9mws8P3g3DCw"
-        avatarUrl="https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s88-c-k-c0x00ffffff-no-rj"
-      />
+      {videos.map((video) => (
+        <VideoRow key={video.id} {...video} />
+      ))}
     </div>
   );
 };
