0a6fcedce468c561da0d8e5dde17371d4648533c
Samer A <thedev.samer@gmail.com>
2022-04-07 17:53:12 +0300

    finished building the VideoRow component

---
+++ src/components/VideoRow.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../css/video-row.css";
+
+const VideoRow = ({
+  posterUrl,
+  channel,
+  title,
+  description,
+  subscribers,
+  views,
+  timestamp,
+}) => {
+  const truncate = (str = "") => {
+    const condition = str.length >= 300;
+    return condition ? str.substring(0, 300) + "..." : str;
+  };
+
+  return (
+    <div className="video-row">
+      <img className="video-row__poster" src={posterUrl} alt="Video poster" />
+
+      <div className="video-row__text">
+        <h3 className="video-row__heading">{title}</h3>
+
+        <p className="video-row__views">
+          <span>{channel}</span>
+          <span>{subscribers} subscribers</span>
+          <span>{views} views</span>
+          {timestamp}
+        </p>
+
+        <p className="video-row__description">{truncate(description)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoRow;
